3cbe565d9939c7e0670ad0fed0903522ade7cd93
Your Name <your-email@example.com>
2026-06-16 14:15:31 +0530

    Test Commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 2;
-        int b = 4;
-        System.out.println(Math.max(a, b));
-        System.out.println(Math.min(a, b));
-        float percentage = 99.99F;
-        System.out.println(Math.round(percentage));
-        System.out.println(Math.floor(percentage));
-        System.out.println(Math.ceil(percentage));
-        int num = 169;
-        System.out.println(Math.sqrt(num));
-
+        double random = Math.random();
+        System.out.println(random * 100);
     }
 }
